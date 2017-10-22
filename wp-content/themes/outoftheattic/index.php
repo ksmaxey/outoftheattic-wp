81291033231ac797143ba605d0ff245d54cd217b
Kyle Maxey <ksmaxey@gmail.com>
2017-10-22 15:14:37 -0400

    add wordpress php to blog page

--- wp-content/themes/outoftheattic/index.php
+++ wp-content/themes/outoftheattic/index.php
@@ -1,19 +1,62 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<!-- PAGE TITLE -->
+			<h1>Blog</h1>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+<?php if (have_posts()) : ?>
+	<!-- CONTENT -->
+	<div class="blog">
+		<div class="blog-container">
+	<?php while (have_posts()) :
 
-			<?php get_template_part('partials/loop'); ?>
+		the_post(); ?>
+				<!-- The blog sections are broken up into large, medium, and small sections -->
+					<div class="blog-large">
+						<h2><?php the_title(); ?></h2>
+						<?php the_post_thumbnail(); ?>
+						<p>Out of the Attic Antiques is searching for individuals that can think on their feet, and are able to utilize provided tools to get the job done. Experience is a plus, but not necessary. The successful applicant must have a burning desire to succeed and an eagerness to learn. We want you to grow with our company where your ideas and suggestions aid in the company’s growth and you are rewarded for it.</p>
+						<a href="<?php the_permalink(); ?>"><button type="button" name="button">Read More</button></a>
+					</div>
+				<?php endwhile; ?>
 
-			<?php get_template_part('partials/pagination'); ?>
+			</div>
+		</div>
 
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+			<?php else: ?>
+				<em>No Posts Found</em>
+			<?php endif; ?>
+					<!--
+					<div class="blog-medium">
+						<div class="blog-medium-content">
+							<h3>New Items</h3>
+							<img src="img/bike.jpg" alt="New Items">
+							<p>We have some great new items in!  Whether you're looking for a new (not really) bike, globe, or dive mask, we have you covered!  Check out some pictures by clicking the read more button.</p>
+						</div>
+						<a href="blog-expand.html"><button type="button" name="button">Read More</button></a>
+					</div>
+					<div class="blog-small" id="item_1">
+						<div class="blog-small-content">
+							<h3>Extremely Valuable</h3>
+							<a href="blog-expand.html"><button type="button" name="button">More</button></a>
+						</div>
+					</div>
+					<div class="blog-small" id="item_2">
+						<div class="blog-small-content">
+							<h3>How To Start</h3>
+							<a href="blog-expand.html"><button type="button" name="button">More</button></a>
+						</div>
+					</div>
+					<div class="blog-small" id="item_3">
+						<div class="blog-small-content">
+							<h3>We're On TV!</h3>
+							<a href="blog-expand.html"><button type="button" name="button">More</button></a>
+						</div>
+					</div>
+					<div class="blog-small" id="item_4">
+						<div class="blog-small-content">
+							<h3>About Our Staff</h3>
+							<a href="blog-expand.html"><button type="button" name="button">More</button></a>
+						</div>
+					</div> -->
 
 <?php get_footer(); ?>
--- wp-content/themes/outoftheattic/single.php
+++ wp-content/themes/outoftheattic/single.php
@@ -1,71 +1,36 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
-
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
-
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
-
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+		<h1>Interested In Working With Us?</h1>
+		<div class="blog-expand-grid">
+			<main>
+				<img src="img/hiring2.jpg" alt="Now Hiring" id="expand-blog-image">
+			</main>
+			<!-- This is the individually scrollable area -->
+			<aside class="scrollable expand-container">
+				<div class="description">
+					<h2>Description:</h2>
+					<ul>
+						<li>Applicant must have Reliable Transportation</li>
+						<li>Must be punctual and able to keep track of all appointments</li>
+						<li>Must demonstrate strong Computer, Tablet, and Phone skills</li>
+						<li>Must have a positive attitude and want to grow with the company</li>
+						<li>Consistent Communication is key</li>
+						<li>Professional Dress</li>
+					</ul>
+				</div>
+				<div class="skills">
+					<h2>These Skills are a Plus:</h2>
+					<ul>
+						<li>Associates Degree in Business/Marketing</li>
+						<li>Public Speaker</li>
+						<li>Great People Skills</li>
+						<li>Antique Knowledge (not required)</li>
+					</ul>
+				</div>
+				<p>If interested in applying, please use the form located on our Contact page.</p>
+			</aside>
+		</div>
+		<!--Button that takes the user back to the blog page -->
+		<a href="blog.html" title="Blog"><button type="button" name="button">Back To Blog</button></a>
 
 <?php get_footer(); ?>
