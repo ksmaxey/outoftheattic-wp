3139f8536142a059476bef71f272b0c493ec5644
Kyle Maxey <ksmaxey@gmail.com>
2017-10-30 19:59:28 -0400

    fix all headings

--- wp-content/themes/outoftheattic/page-about.php
+++ wp-content/themes/outoftheattic/page-about.php
@@ -5,7 +5,7 @@ Template Name: About
 get_header(); ?>
 
 <div class="about-container">
-    <main>
+    <main class="about">
       <h1>About</h1>
       <p>Like many antique lovers, owner Glenn Lynch started out as a collector and when the opportunity knocked to open his own store,  he opened the door and never looked back. Now with over 75 dealers, the store is overflowing with everything from turn-of-the-century furniture to 45 records, from itty-bitty glove buttons of the 1930's to 1950's kitsch, from Depression glass to picnic baskets. The list goes on and on.<br><br>
       Glenn's passion for Brewerania began in his basement and soon moved to store shelves, including English water pitchers, shot glasses, beer taps, steins, liquor decanters, whiskey pourers and beer glasses of all shapes and sizes. Julie's collections include tins, vintage buttons and Depression glass.<br><br>
@@ -13,7 +13,7 @@ get_header(); ?>
       Make it a family outing! Hope to see you soon.<br><br>
       Best,<br><span>Glenn</span></p> <!--This span is to represent a signature -->
     </main>
-    <aside>
+    <aside class="about">
       <!--Virtual Tour through the store that is linked to Google -->
       <h2>Virtual Tour</h2>
       <iframe width="700" height="394" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps?layer=c&amp;sll=41.759426,-88.21834899999999&amp;cid=97793693872733656&amp;panoid=8cvm2MKTPjQAAAQW44G2vQ&amp;cbp=13,349.19,,0,0&amp;q=out+of+the+attic+antiques+aurora&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;t=m&amp;cbll=41.760372,-88.218657&amp;source=embed&amp;ll=41.75856,-88.218648&amp;spn=0.006307,0.01502&amp;z=16&amp;output=svembed"></iframe>
--- wp-content/themes/outoftheattic/page-featured.php
+++ wp-content/themes/outoftheattic/page-featured.php
@@ -3,91 +3,92 @@
 Template Name: Featured
 */
 get_header(); ?>
-
-<section class="section">
-    <h1>Featured Items</h1>
-    <div class="grid">
-      <div class="item">
-        <div class="item__details">
-          1942 Gibson L7 Archtop Guitar
+<main class="featured">
+  <section class="section">
+      <h1>Featured Items</h1>
+      <div class="grid">
+        <div class="item">
+          <div class="item__details">
+            1942 Gibson L7 Archtop Guitar
+          </div>
+        </div>
+        <div class="item item--large">
+          <div class="item__details">
+            Mark V Dive Helmet
+          </div>
+        </div>
+        <div class="item item--medium">
+          <div class="item__details">
+            1888 Columbia Phonograph
+          </div>
+        </div>
+        <div class="item item--large">
+          <div class="item__details">
+            1950 Midwest Television
+          </div>
+        </div>
+        <div class="item item--full">
+          <div class="item__details">
+             Extremely Rare Train Set
+          </div>
+        </div>
+        <div class="item item--medium">
+          <div class="item__details">
+            Vintage Rolex Oyster Watch
+          </div>
+        </div>
+        <div class="item item--large">
+          <div class="item__details">
+            World War Two Bomber Flight Jacket
+          </div>
+        </div>
+        <div class="item">
+          <div class="item__details">
+            Eames Lounge Chair
+          </div>
+        </div>
+        <div class="item item--medium">
+          <div class="item__details">
+            Beatles Autographed Photograph
+          </div>
+        </div>
+        <div class="item item--large">
+          <div class="item__details">
+            Antique Coffee Grinder
+          </div>
+        </div>
+        <div class="item">
+          <div class="item__details">
+            Old Yunegling Beer Cans
+          </div>
+        </div>
+        <div class="item item--medium">
+          <div class="item__details">
+            Coronelli's 1688 Terrestrial Globe
+          </div>
+        </div>
+        <div class="item item--medium">
+          <div class="item__details">
+            1930s Depression Glass Dish
+          </div>
+        </div>
+        <div class="item item--large">
+          <div class="item__details">
+            Antique German Picnic Basket
+          </div>
+        </div>
+        <div class="item">
+          <div class="item__details">
+            Victorian Three Stone Diamond Ring
+          </div>
+        </div>
+        <div class="item">
+          <div class="item__details">
+            19th Century Mahogany Sofa
+          </div>
         </div>
       </div>
-      <div class="item item--large">
-        <div class="item__details">
-          Mark V Dive Helmet
-        </div>
-      </div>
-      <div class="item item--medium">
-        <div class="item__details">
-          1888 Columbia Phonograph
-        </div>
-      </div>
-      <div class="item item--large">
-        <div class="item__details">
-          1950 Midwest Television
-        </div>
-      </div>
-      <div class="item item--full">
-        <div class="item__details">
-           Extremely Rare Train Set
-        </div>
-      </div>
-      <div class="item item--medium">
-        <div class="item__details">
-          Vintage Rolex Oyster Watch
-        </div>
-      </div>
-      <div class="item item--large">
-        <div class="item__details">
-          World War Two Bomber Flight Jacket
-        </div>
-      </div>
-      <div class="item">
-        <div class="item__details">
-          Eames Lounge Chair
-        </div>
-      </div>
-      <div class="item item--medium">
-        <div class="item__details">
-          Beatles Autographed Photograph
-        </div>
-      </div>
-      <div class="item item--large">
-        <div class="item__details">
-          Antique Coffee Grinder
-        </div>
-      </div>
-      <div class="item">
-        <div class="item__details">
-          Old Yunegling Beer Cans
-        </div>
-      </div>
-      <div class="item item--medium">
-        <div class="item__details">
-          Coronelli's 1688 Terrestrial Globe
-        </div>
-      </div>
-      <div class="item item--medium">
-        <div class="item__details">
-          1930s Depression Glass Dish
-        </div>
-      </div>
-      <div class="item item--large">
-        <div class="item__details">
-          Antique German Picnic Basket
-        </div>
-      </div>
-      <div class="item">
-        <div class="item__details">
-          Victorian Three Stone Diamond Ring
-        </div>
-      </div>
-      <div class="item">
-        <div class="item__details">
-          19th Century Mahogany Sofa
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
+  </main>
 
 <?php get_footer(); ?>
