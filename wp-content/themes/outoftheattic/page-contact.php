2b98afbb2386b37c39784b838698708b0c8fa5f0
Kyle Maxey <ksmaxey@gmail.com>
2017-11-03 19:22:35 -0400

    add contact form php

--- wp-content/themes/outoftheattic/page-contact.php
+++ wp-content/themes/outoftheattic/page-contact.php
@@ -4,34 +4,6 @@ Template Name: Contact
 */
 get_header(); ?>
 
-<main class="contact">
-  <h1>Contact</h1>
-      <div class="form-container">
-        <form action="mailto:ksmaxey@gmail.com" method="post" id="form" autocomplete="on">
-          <div class="form-name">
-            <input type="text" name="name" placeholder="Name:" id="name" size="25" maxlength="25"/>
-          </div>
-          <div class="form-email">
-            <input type="email" name="email" placeholder="Email:" id="email" size="50" maxlength="50"/>
-          </div>
-          <div class="form-message">
-            <textarea name="message" id="message" placeholder="Message:" rows="10" cols="30"></textarea>
-          </div>
-          <!-- User will get a pop up message once this button is clicked -->
-          <div class="form-button">
-            <button type="button" name="button" id="myBtn">Send</button>
-          </div>
-        </form>
-        <!-- Modal -->
-        <div id="myModal" class="modal">
-        <!-- Modal Content -->
-        <div class="modal-content">
-          <span class="close">&times;</span>
-          <p>Thank You for contacting us, we'll be in touch soon!</p>
-        </div>
-      </div>
-    </div>
-  </main>
-
+  <?= the_content(); ?>
 
 <?php get_footer(); ?>
